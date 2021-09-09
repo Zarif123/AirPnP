1d354f565c77208a235a2c3a460639ec06fbff0c
Zarif Ceaser <zarifceaser321@gmail.com>
2021-09-09 00:41:54 -0400

    Frontend and Models

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {
+    Form,
+    Select,
+    InputNumber,
+    Switch,
+    Radio,
+    Slider,
+    Button,
+    Upload,
+    Rate,
+    Checkbox,
+    Row,
+    Col,
+    Typography
+} from 'antd';
+
+const { Title } = Typography;
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    {/*<Title level={2}>Estimated Price: </Title>*/}
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
